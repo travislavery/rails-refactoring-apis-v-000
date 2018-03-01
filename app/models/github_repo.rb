501d091d4cfc63769b92314e060527d2d1996d85
TravisA <github email address>
2018-02-28 18:22:51 -0700

    Done.

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -2,9 +2,9 @@ class GithubRepo
 
   attr_reader :name, :url
 
-  def initialize(hash)
-    @name = hash["name"]
-    @url = hash["html_url"]
+  def initialize(git_hash)
+    @name = git_hash["name"]
+    @url = git_hash["html_url"]
   end
 
 end
